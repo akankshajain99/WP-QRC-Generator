1a3e01eb3e26055c3c275a1dc667089f1e4d20e2
Akanksha Jain <akankshajain2299@gmail.com>
2025-05-02 23:48:52 +0530

    shortcode template for backend

---
+++ template/qrc_shortcode.php
@@ -0,0 +1,6 @@
+<div class="wrap">
+	<h2>QRC Shortcode</h2>
+	<h3 id="qrcg_st">[qr_code_generator]</h3>
+	<h4>Copy above shortcode and paste it any frontend page where you want QR Code Generator</h4>
+</div>
+<hr class="wp-header-end">
