e9afb980e2870aea7deee99d29754efd35b430cd
Akanksha Jain <akankshajain2299@gmail.com>
2025-05-02 23:51:55 +0530

    script file for form sumbit

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function () {
+
+	jQuery('#qr_frm').on("submit", function(event){
+
+		event.preventDefault();
+
+		var formData = new FormData(this);
+		var url = formData.get("url");
+		var qrcode = new QRCode("qrcode", url);
+		document.getElementById("qr_frm").reset();
+
+		
+	});
+});
