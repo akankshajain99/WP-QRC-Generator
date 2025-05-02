3fea3fb77edbaafd1f44d7fe20bd9747349a8a55
Akanksha Jain <akankshajain2299@gmail.com>
2025-05-02 23:39:00 +0530

    add form template

---
+++ template/qrc_form.php
@@ -0,0 +1,15 @@
+<header>
+    <h4>QR Code Generator for any Website or any URL</h4>
+</header>
+<main>
+	<form method="post" action="javascript:void(0)" id="qr_frm">
+		<div class="form-group">
+			<input type="url" name="url" id="url" class="form-control" />
+		</div>
+		<div class="form-group">
+            <button type="submit" name="url_submit" class="btn btn-primary btn-block">Submit URL</button>
+        </div>
+		
+	</form>
+    <div id="qrcode" class="mt-5"></div>
+</main>
